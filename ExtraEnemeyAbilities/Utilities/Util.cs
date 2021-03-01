ccf01dd6c83165e3538d71ce6638f029d50f3298
Dak <vectorskeleton@gmail.com>
2021-03-01 02:40:00 -0800

    Update Util.cs

--- ExtraEnemeyAbilities/Utilities/Util.cs
+++ ExtraEnemeyAbilities/Utilities/Util.cs
@@ -11,6 +11,11 @@ namespace ExtraEnemyAbilities.Utilities
     {
         public static Color GetUnityColor(ColorData cd)
         {
+            if (cd.r == 0 && cd.b == 0 && cd.g == 0 && cd.a == 0)
+            {
+                return new Color(1, 0, 0, 1);
+            }
+
             return new Color(cd.r, cd.g, cd.b, cd.a);
         }
     }
